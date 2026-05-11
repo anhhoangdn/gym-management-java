a9c1d2aa5f1a1afdb21f57b1bed522f321c16a09
tsown14 <sonphantan410106@gmail.com>
2026-05-11 18:49:39 +0700

    Code các chức năng trong Controller.

--- src/controller/Quit.java
+++ src/controller/Quit.java
@@ -5,12 +5,10 @@ import util.Operation;
 public class Quit implements Operation {
 
     public Quit() {
-        // TODO: initialize any required dependencies before quitting
     }
 
     @Override
     public void execute() {
-        // TODO: close the database connection, dispose all windows,
-        //       and exit the application cleanly
+        System.exit(0);
     }
 }
--- src/controller/ShowAllRegistrations.java
+++ src/controller/ShowAllRegistrations.java
@@ -1,20 +1,41 @@
 package controller;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+import model.Registration;
+import repository.RegistrationRepository;
 import util.Operation;
 import view.RegistrationView;
+import javax.swing.table.DefaultTableModel;
 
 public class ShowAllRegistrations implements Operation {
 
-    private RegistrationView registrationView;
+    private final RegistrationRepository regRepo;
 
-    public ShowAllRegistrations() {
-        // TODO: initialize RegistrationView and any required dependencies
-        this.registrationView = new RegistrationView();
+    public ShowAllRegistrations(RegistrationRepository regRepo) {
+        this.regRepo = regRepo;
     }
 
     @Override
     public void execute() {
-        // TODO: fetch all registrations from the database and
-        //       display them using registrationView.showRegistrationList()
+        RegistrationView view = RegistrationView.showAllList();
+        DefaultTableModel model = view.getTableModel();
+        model.setRowCount(0);
+
+        List<Registration> list = regRepo.findAll();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        for (Registration reg : list) {
+            Object[] row = {
+                reg.getId(),
+                reg.getUserId(),
+                reg.getPackageId(),
+                reg.getStartDate() != null ? sdf.format(reg.getStartDate()) : "",
+                reg.getEndDate() != null ? sdf.format(reg.getEndDate()) : "",
+                reg.getTotal(),
+                reg.getStatusLabel()
+            };
+            model.addRow(row);
+        }
     }
 }
